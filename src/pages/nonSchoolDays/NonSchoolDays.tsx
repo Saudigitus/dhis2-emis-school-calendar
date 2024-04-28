88800f4c81ecb69bd0781efe51ffce4a03334a43
Eddy-Nhabinde <edmilsonnhabinde5@gmail.com>
2024-04-28 20:03:44 +0200

    [EMISWEB-105] added component to list non school days

--- src/pages/nonSchoolDays/NonSchoolDays.tsx
+++ src/pages/nonSchoolDays/NonSchoolDays.tsx
@@ -1,12 +1,33 @@
 import React from 'react'
-import { WithPadding } from '../../components'
+import {WithPadding} from '../../components'
+import style from './NonSchoolDays.module.css'
+import OffDaysCard from "../../components/card/CardComponent";
+import {Paper} from "@material-ui/core";
 
 const NonSchoolDays = () => {
-  return (
-    <WithPadding padding="10px 30px">
-      <div>NonSchoolDays</div>
-    </WithPadding>
-  )
+    const arr = [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1]
+    return (
+        <WithPadding padding="10px 30px">
+            <div className={style.title}>Non School Days</div>
+            <div className={style.container}>
+                <div className={style.listContainer}>
+                    <span className={style.sectionTitle}>Non School Days List</span>
+                    <div className={style.list}>
+                        {
+                            arr.map(()=>{
+                                return <div>
+                                    <OffDaysCard offDayType={'Public Holiday'} title={'Independence Day'} date={'2024-10-10'}/>
+                                </div>
+                            })
+                        }
+                    </div>
+                </div>
+                <div className={style.formContainer}>
+                    <span className={style.sectionTitle} >Add new off day</span>
+                </div>
+            </div>
+        </WithPadding>
+    )
 }
 
 export default NonSchoolDays
