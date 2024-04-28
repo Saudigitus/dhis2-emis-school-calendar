1a5645ec137a2eeffab82fa88eef909cec9be439
Eddy-Nhabinde <edmilsonnhabinde5@gmail.com>
2024-04-28 22:50:34 +0200

    [EMISWEB-105] removed unused code

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -2,15 +2,15 @@ import React from "react";
 import style from "./Home.module.css";
 import { DashboardCard, Title, WithPadding } from "../../components";
 import {menuData} from "../../utils/index"
-import { MenuDataItemProps, MenuDataProps } from "../../types/menu/MenuTypes";
+import { type MenuDataItemProps, type MenuDataProps } from "../../types/menu/MenuTypes";
 import { getDataStoreKeys } from "../../utils/common/dataStore/getDataStoreKeys";
 
 function Home(): React.ReactElement {
     const { currentAcademicYear } = getDataStoreKeys()
-    
+
     return (
     <WithPadding padding="10px 30px">
-      {menuData(currentAcademicYear).filter(item => item.displayInDashboard === true).map((section:MenuDataProps, y) => (
+      {menuData(currentAcademicYear).filter(item => item.displayInDashboard === true).map((section: MenuDataProps, y) => (
         <div key={y} className={style.section}>
           <Title label={section.title}/>
           <div className={style.containerCards}>
