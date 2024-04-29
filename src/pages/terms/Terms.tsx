f6b459512343d3459722f1552855c93edf2a182d
cnandza <clayd.nandza@saudigitus.org>
2024-04-29 10:41:41 +0200

    EMISWEB-107 - Terms form created

--- src/pages/terms/Terms.tsx
+++ src/pages/terms/Terms.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { IconUserGroup16 } from '@dhis2/ui'
 import { WithPadding } from '../../components'
 import TermForm from '../../components/forms/TermForm'
 import DropdownButtonComponent from '../../components/buttons/DropdownButton'
@@ -6,11 +7,11 @@ import DropdownButtonComponent from '../../components/buttons/DropdownButton'
 function Terms() {
   //USE SOMETHING TO GET ROUTE TERM NUMBER/ID TO RENDER CONDITIONALLY
   // const { } = use
-  
+
   return (
     <WithPadding padding="10px">
-      <DropdownButtonComponent name='Term' options={[]} disabled={false}/>
-      <TermForm/>
+      <DropdownButtonComponent name='Term' options={[]} disabled={false} icon={<IconUserGroup16 />} />
+      <TermForm />
     </WithPadding>
 
   )
