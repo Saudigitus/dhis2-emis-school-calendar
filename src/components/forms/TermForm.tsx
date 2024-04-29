20f4b8b7e13f04d9cf58a2f948fde671b15e7527
cnandza <clayd.nandza@saudigitus.org>
2024-04-29 14:24:53 +0200

    EMISWEB-107 adding action buttons

--- src/components/forms/TermForm.tsx
+++ src/components/forms/TermForm.tsx
@@ -1,27 +1,40 @@
-import React, { useRef } from "react";
+import React, { useRef, useState } from "react";
 import style from "./termForm.module.css";
 import { WithPadding } from "../template";
-import { termsFormData } from "../../utils/constants/termsFormData";
+import { termsFormData, termsInitalValues } from "../../utils/constants/termsFormData";
 import { FormSectionProps } from "../../types/form/FormSectionProps";
 import GroupForm from "../groupForm/GroupForm";
 import { Form } from "react-final-form"
+import { ButtonStrip, Button } from '@dhis2/ui'
 
 function TermForm(): React.ReactElement {
     const formRef: React.MutableRefObject<FormApi<IForm, Partial<IForm>>> = useRef(null);
+    const [initialValues] = useState<object>({ ...termsInitalValues })
+    const [values, setValues] = useState<Record<string, string>>({...termsInitalValues})
+    const [fieldsWitValue, setFieldsWitValues] = useState<any[]>([])
+    const [disabled, setDisabled] = useState<boolean>(true)
 
+    const modalActions = [
+        { id: "enable", type: "button", label: "Edit", primary: true, disabled: false, onClick: () => { setDisabled(!disabled) }, display: disabled },
+        { id: "cancel", type: "button", label: "Cancel", secondary: true, disabled: false, onClick: () => { setDisabled(!disabled) }, display: !disabled },
+        { id: "save", type: "submit", label: "Save", primary: true, disabled: false, onClick: () => { setDisabled(!disabled) }, display: !disabled }
+    ];
     function onSubmit() { }
-    function onChange() { }
+
+    function onChange(changeValues: any) {
+        setValues(changeValues)
+    }
 
     return (
         <WithPadding padding="5px">
-            <Form initialValues={{}} onSubmit={onSubmit}>
+            <Form initialValues={{ ...initialValues }} onSubmit={onSubmit}>
                 {({ handleSubmit, values, form }) => {
                     formRef.current = form;
                     return <form
                         onSubmit={handleSubmit}
-                        onChange={onChange() as unknown as () => void}
+                        onChange={onChange(values) as unknown as () => void}
                     >
-                        {termsFormData()?.map((section: FormSectionProps, index: number) => {
+                        {termsFormData(disabled)?.map((section: FormSectionProps, index: number) => {
                             return (
                                 <GroupForm
                                     key={index}
@@ -31,8 +44,27 @@ function TermForm(): React.ReactElement {
                                 />
                             )
                         })}
+                        <ButtonStrip start className="mr-4">
+                            {modalActions?.filter((action)=> action.display).map((action, i) => {
+                                return (
+                                    <>
+                                        {
+
+                                            <Button
+                                                key={i}
+                                                loading={false}
+                                                {...action}
+                                            >
+                                                {action.label}
+                                            </Button>
+                                        }
+                                    </>
+                                )
+                            })}
+                        </ButtonStrip>
                     </form>
                 }}
+
             </Form>
         </WithPadding>
     );
