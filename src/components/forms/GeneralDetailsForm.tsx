3a3eb186a8e3fe7bf50881895cf6ffd4a70e3928
Eddy-Nhabinde <edmilsonnhabinde5@gmail.com>
2024-05-15 11:45:24 +0200

    [EMISWEB-115] Solved: when changing values ​​in inputs, onchange only returned previous and not updated values

--- src/components/forms/GeneralDetailsForm.tsx
+++ src/components/forms/GeneralDetailsForm.tsx
@@ -1,20 +1,20 @@
-import React, {useRef} from "react";
-import {WithPadding} from "../template";
-import {generalDetailsFormData} from "../../utils/constants/generalDetailsFormData";
-import {type FormSectionProps} from "../../types/form/FormSectionProps";
+import React, { useRef, useEffect } from "react";
+import { WithPadding } from "../template";
+import { generalDetailsFormData } from "../../utils/constants/generalDetailsFormData";
+import { type FormSectionProps } from "../../types/form/FormSectionProps";
 import GroupForm from "../groupForm/GroupForm";
-import {Form} from "react-final-form"
-import {useRecoilValue} from "recoil";
-import {DataStoreState} from "../../schema/dataStoreSchema";
-import {dataStoreManagement} from "../../hooks/dataStore/useDSManagement";
-import {type dataStoreRecord} from "../../types/dataStore/DataStoreConfig";
+import { Form } from "react-final-form"
+import { useRecoilValue } from "recoil";
+import { DataStoreState } from "../../schema/dataStoreSchema";
+import { dataStoreManagement } from "../../hooks/dataStore/useDSManagement";
+import { type dataStoreRecord } from "../../types/dataStore/DataStoreConfig";
 
 function GeneralDetailsForm(): React.ReactElement {
     const formRef: React.MutableRefObject<FormApi<IForm, Partial<IForm>>> = useRef(null);
     const dataStoreData = useRecoilValue(DataStoreState)
-    const {postData} = dataStoreManagement()
+    const { postData } = dataStoreManagement()
 
-    function getValues(formValues: dataStoreRecord['weekDays'] | dataStoreRecord ['academicYear'], dataStoreKey: any) {
+    function getValues(formValues: dataStoreRecord['weekDays'] | dataStoreRecord['academicYear'], dataStoreKey: any) {
         const updatedValues: any = {}
 
         Object.keys(dataStoreKey).forEach((key: any) => {
@@ -27,30 +27,27 @@ function GeneralDetailsForm(): React.ReactElement {
         return updatedValues
     }
 
-    function onChange(e: any, active: any) {
+    function onChange(e: any) {
         void postData({
             ...dataStoreData,
             weekDays: getValues(e, dataStoreData.weekDays),
             academicYear: getValues(e, dataStoreData.academicYear)
-        }, dataStoreData.academicYear?.[active] ? 'Data saved successfuly' : null)
+        }, 'Data updated successfuly')
     }
 
     return (
         <WithPadding padding="5px">
-            <Form initialValues={{...dataStoreData.weekDays, ...dataStoreData.academicYear}} onSubmit={() => {
+            <Form initialValues={{ ...dataStoreData.weekDays, ...dataStoreData.academicYear }} onSubmit={() => {
             }}>
-                {({
-                      handleSubmit,
-                      values,
-                      form,
-                    active
-                  }) => {
+                {({ handleSubmit, values, form, initialValues }) => {
                     formRef.current = form;
+
+                    useEffect(() => {
+                        if (JSON.stringify(initialValues) !== JSON.stringify(values)) onChange(values)
+                    }, [values])
+
                     return <form
                         onSubmit={handleSubmit}
-                        onChange={() => {
-                            onChange(values, active)
-                        }}
                     >
                         {generalDetailsFormData()?.map((section: FormSectionProps, index: number) => {
                             return (
