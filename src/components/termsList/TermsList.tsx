3b46aaae5470dd8b6d74ac9f561a7581b8ded9d1
cnandza <cnandza@saudigitus.org>
2025-07-30 15:49:07 +0200

    add onclose modal properly to modals

--- src/components/termsList/TermsList.tsx
+++ src/components/termsList/TermsList.tsx
@@ -19,7 +19,7 @@ function TermsList() {
 
     return (
         <div>
-            <ModalComponent onClose={setOpen} open={open} title={'Non School Day Register'} children={<NewSchoolTerm setOpen={setOpen} />} />
+            <ModalComponent onClose={() => setOpen(false)} open={open} title={'Non School Day Register'} children={<NewSchoolTerm setOpen={setOpen} />} />
             <WithPadding padding='10px'>
                 <Button
                     variant="outlined"
